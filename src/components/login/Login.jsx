95c31277fd8319f674e6e402dd99743473c7e697
Eduardo <eduardokunmixyeah@gmail.com>
2023-10-17 23:46:22 -0400

    fix login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,7 @@ import './login.css'
 import { useState } from 'react';
 import Button from '@mui/material/Button';
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
+// import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
@@ -10,10 +10,10 @@ import InputAdornment from '@mui/material/InputAdornment';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import FormHelperText from '@mui/material/FormHelperText';
+// import Box from '@mui/material/Box';
+// import Input from '@mui/material/Input';
+// import FilledInput from '@mui/material/FilledInput';
+// import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 
 export const Login = () => {
@@ -21,6 +21,7 @@ export const Login = () => {
     const [pasword,setPassword] = useState('');
 
     const [error,setError] = useState(false);
+    const [errorMessage,setErrorMessage] = useState('Todos los campos son obligatorios');
 
     const [showPassword, setShowPassword] = React.useState(false);
 
@@ -30,19 +31,27 @@ export const Login = () => {
         event.preventDefault();
     };
 
-    let pass = true;
-
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if(nombre === '' || pasword === ''){
+            setError(true);
+            setErrorMessage('Todos los campos son obligatorios');
+            return
+        }
+
+        if(nombre !== 'admin' && pasword !== 'admin'){
+            setErrorMessage('Usuario Invalido');
             setError(true)
             return
         }
 
+        if(nombre === 'admin' && pasword === 'admin'){
+            location.href='/home';
+            setError(true)
+            return
+        }
         setError(false)
-
-        location.href='/home'
     }
 
     return (
@@ -93,9 +102,12 @@ export const Login = () => {
             </FormControl>
 
 
-            <Button variant="outlined">Iniciar Sesión</Button>
+            <Button 
+            variant="outlined"
+            onClick={handleSubmit}
+            >Iniciar Sesión</Button>
+        {error && <p className='errorMessage'>{errorMessage}</p>}
         </form>
-        {error && <p>Todos los campos son obligatorios</p>}
     </>
     )
 };
