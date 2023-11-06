35cb20f991cef3f4fa06ede74afe188aa28810a9
Eduardo282x <eduardokunmixyeah@gmail.com>
2023-11-06 13:17:25 -0400

    add table in users and add styles

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,11 +2,26 @@ import './users.css'
 import { Button } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from "react-router-dom";
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export const Users = () => {
 
     const navigate = useNavigate();
+
+    function createData(name, lastname, email, id, phone) {
+        return { name, lastname, email, id, phone };
+    }
+
+    const rows = [
+        createData('admin', 'admin', 'email', '28391325', '+58 4165610813'),
+    ];
+    
 
     const goBack = () => {
         navigate('/home')
@@ -14,10 +29,41 @@ export const Users = () => {
     
         return (
         <div className="log">
-            <Button variant="contained" onClick={goBack}>
-                <ArrowBackIcon className='back'/>
-            </Button>
-            <h1>Usuarios </h1>
+            <Paper elevation={24} className='paperWidh'>
+                <div className="buttonBack">
+                    <Button variant="contained" onClick={goBack}>
+                        <ArrowBackIcon className='back'/>
+                    </Button>
+                    <h1>Usuarios</h1>
+                </div>
+
+                <div className="tableContent">
+                    <TableContainer component={Paper } sx={{ maxWidth: 650 }} className='table'>
+                        <Table sx={{ maxWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>Nombre</TableCell>
+                                <TableCell>Apellido</TableCell>
+                                <TableCell>Correo</TableCell>
+                                <TableCell>Cedula</TableCell>
+                                <TableCell>Telefono</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row.name} >
+                                    <TableCell>{row.name}</TableCell>
+                                    <TableCell>{row.lastname}</TableCell>
+                                    <TableCell>{row.email}</TableCell>
+                                    <TableCell>{row.id}</TableCell>
+                                    <TableCell>{row.phone}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+            </Paper>
         </div>
     )
 }
