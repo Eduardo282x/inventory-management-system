b428a11494ebb9e054429cf3e6c49b41d6b89e63
Eduardo282x <eduardokunmixyeah@gmail.com>
2023-11-20 14:46:22 -0400

    add input in profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,6 +2,10 @@ import './profile.css'
 import { Button } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from "react-router-dom";
+import PersonIcon from '@mui/icons-material/Person';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
 export const Profile = () => {
   const navigate = useNavigate();
@@ -10,12 +14,32 @@ export const Profile = () => {
       navigate(-1);
   }
 
+  const userData = JSON.parse(localStorage.getItem('userData'))
+
+  const [nombre,setNombre] = useState(userData.nombre)
+
+  console.log(userData);
+
+  // setTimeout(() => {
+  //   setNombre(userData.nombre)
+  // }, 1500);
+
   return (
-    <div className="log"> 
-      <Button variant="contained" onClick={goBack}>
-            <ArrowBackIcon className='back'/>
-        </Button>
-      <h1>Perfil </h1>
-    </div>
+    <div className="log">
+    <Paper elevation={24} className='profileWidh'>
+        <div className="profileContent">
+            <div className="profileCenter">
+                <Button variant="contained" onClick={goBack}>
+                    <ArrowBackIcon className='back'/>
+                </Button>
+                <h1><PersonIcon className='groupIcon'/>Perfil</h1>
+            </div>
+
+            <div className="profileInfo">
+              <TextField id="outlined-basic" label="Nombre" value={nombre} variant="outlined" />
+            </div>
+        </div>
+    </Paper>
+</div>
   )
 }
