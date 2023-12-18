91fdf085b168533c0c5b4038f6b41dfb53adbe49
Eduardo <eduardokunmixyeah@gmail.com>
2023-12-18 19:40:54 -0400

    add axios, add snackbar, change login, conected api

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
+import * as React from "react";
 import "./login.css";
 import { useState } from "react";
 import Button from "@mui/material/Button";
-import * as React from "react";
 import IconButton from "@mui/material/IconButton";
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
@@ -12,55 +12,70 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Icon from "@mui/material/Icon";
 import FormControl from '@mui/material/FormControl';
 import { useNavigate } from "react-router-dom";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import axios from '../../env/axios-instance';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export const Login = () => {
     const [loginData, setLoginData] = useState({
-        nombre: "",
-        password: "",
+        Username: "",
+        Password: "",
     });
+
+    const position = {
+        open: false,
+        vertical: 'bottom',
+        horizontal: 'center',
+    };
+    
     const navigate = useNavigate();
+    const [error, setError] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState("");
+    const [messageType, setMessageType] = useState("");
+    const [showPassword, setShowPassword] = React.useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const dataLogin = (e) => {
         setLoginData({ ...loginData, [e.target.name]: e.target.value });
     };
 
-    const [error, setError] = useState(false);
-    const [showPassword, setShowPassword] = React.useState(false);
-
-    const [errorMessage, setErrorMessage] = useState(
-        "Todos los campos son obligatorios"
-    );
-
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
     };
+
+    const handleMouseDownPassword = (event) => {event.preventDefault();};
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(loginData);
+        axios.post('/authentication', loginData).then((response) => {
+            setOpen(true);
+            setMessageType(response.data.success ? 'success' : 'error');
+            if(response && response.data.success){
+                localStorage.setItem('userData', JSON.stringify(response.data.userData));
+                setMessage(response.data.message);
+                setTimeout(() => {
+                    navigate("/home");
+                }, 1500);
+            }
+            else {
+                setMessage(response.data.message);
+            }
+        })
 
-        if (loginData.nombre === "" || loginData.password === "") {
+        if (loginData.Username === "" || loginData.Password === "") {
             setError(true);
-            setErrorMessage("Todos los campos son obligatorios");
             return;
         }
 
-        if (loginData.nombre !== "admin" && loginData.password !== "admin") {
-            console.log(loginData);
-            setErrorMessage("Usuario Invalido");
-            setError(true);
-            return;
-        }
-
-        if (loginData.nombre === "admin" && loginData.password === "admin") {
-            console.log(loginData);
-            localStorage.setItem('userData', JSON.stringify(loginData))
-            navigate("/home");
-            setError(false);
-            return;
-        }
         setError(false);
     };
 
@@ -69,54 +84,60 @@ export const Login = () => {
             <form className="login" onSubmit={handleSubmit}>
                 <h1>Login</h1>
 
-            <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
-                <InputLabel htmlFor="inputUser">Usuario</InputLabel>
-                <OutlinedInput
-                    id="inputUser"
-                    type="text" 
-                    label="Usuario"
-                    value={loginData.nombre}
-                    name="nombre"
-                    onChange={dataLogin}
-                    endAdornment={
-                    <InputAdornment position="end">
-                        <Icon>
-                        {<AccountCircleIcon />}
-                        </Icon>
-                    </InputAdornment>
-                    }
-                />
-            </FormControl>
+                <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
+                    <InputLabel htmlFor="inputUser">Usuario</InputLabel>
+                    <OutlinedInput
+                        id="inputUser"
+                        type="text" 
+                        label="Usuario"
+                        value={loginData.Username}
+                        name="Username"
+                        onChange={dataLogin}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <Icon>
+                            {<AccountCircleIcon />}
+                            </Icon>
+                        </InputAdornment>
+                        }
+                    />
+                </FormControl>
 
-            <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
-                <OutlinedInput
-                    label="Contraseña"
-                    id="outlined-adornment-password"
-                    type={showPassword ? 'text' : 'password'}
-                    value={loginData.password}
-                    name="password"
-                    onChange={dataLogin}
-                    endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                        >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                    </InputAdornment>
-                    }
-                />
-            </FormControl> 
+                <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
+                    <OutlinedInput
+                        label="Contraseña"
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        value={loginData.Password}
+                        name="Password"
+                        onChange={dataLogin}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        }
+                    />
+                </FormControl> 
 
                 <Button variant="outlined" onClick={handleSubmit}>
                     Iniciar Sesión
                 </Button>
-                {error && <p className="errorMessage">{errorMessage}</p>}
+                {error && <p className="errorMessage">Todos los campos son obligatorios</p>}
             </form>
+
+            <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} anchorOrigin={position}>
+                <Alert onClose={handleClose} severity={messageType} sx={{ width: '100%' }}>
+                    {message}
+                </Alert>
+            </Snackbar>
         </div>
     );
 };
